f164e829d89c39881b8e8a93028b9a5ad1ebf11c
Kim Wijk <kim.wijk@schibsted.se>
2017-02-17 00:23:40 +0100

    Remove rewrite cond

--- server/utils/modRewrite.js
+++ server/utils/modRewrite.js
@@ -3,9 +3,6 @@ export const modRewrite = (req, res, next) => {
     const newHost = req.headers.host.slice(4);
     return res.redirect(301, `${req.protocol}://${newHost}${req.originalUrl}`);
   }
-  if (req.headers.host === '95.85.61.66') {
-    return res.redirect(301, 'http://kimwijk.com');
-  }
   if (req.headers.host.slice(0, 11) === '95.85.61.66') {
     return res.redirect(301, `${req.protocol}://kimwijk.com${req.originalUrl}`);
   }
